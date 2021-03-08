abd2db423078234c8693d1f525ab5a1c4ea8587b
kennencool <3304192108@qq.com>
2021-03-08 15:53:39 +0800

    springboot整合mybatis

---
+++ springboot-mybatis/src/main/java/com/kennen/springbootmybatis/common/dao/repository/UserMapper.java
@@ -0,0 +1,11 @@
+package com.kennen.springbootmybatis.common.dao.repository;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.kennen.springbootmybatis.common.dao.entity.User;
+
+/**
+ * 后台管理用户表 Mapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+    
+}
